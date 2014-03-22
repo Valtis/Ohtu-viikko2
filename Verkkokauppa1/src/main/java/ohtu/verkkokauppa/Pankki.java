800297881b78192f9dc0378bf94c6ad2deac14b0
Valtis <erkka.kaaria@helsinki.fi>
2014-03-22 14:11:24 +0200

    Poistettiin singletonit

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -2,18 +2,10 @@ package ohtu.verkkokauppa;
 
 public class Pankki implements IPankki {
 
-    private static Pankki instanssi;
-
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
+    
     private Kirjanpito kirjanpito;
 
-    private Pankki() {
+    public Pankki() {
         kirjanpito = Kirjanpito.getInstance();
     }
 
