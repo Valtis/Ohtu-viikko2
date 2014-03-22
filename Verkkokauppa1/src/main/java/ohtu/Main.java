2b0c17fda5ec69e26169d791e8a9636d3748e4c8
Valtis <erkka.kaaria@helsinki.fi>
2014-03-22 14:27:22 +0200

    Mainissa nyt vain yksi instanssi kirjanpidosta

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -9,9 +9,10 @@ import ohtu.verkkokauppa.Viitegeneraattori;
 public class Main {
 
     public static void main(String[] args) {
+        Kirjanpito kirjanpito = new Kirjanpito();
         Kauppa kauppa = new Kauppa(
-                new Varasto(new Kirjanpito()), 
-                new Pankki(new Kirjanpito()), 
+                new Varasto(kirjanpito), 
+                new Pankki(kirjanpito), 
                 new Viitegeneraattori());
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
@@ -31,7 +32,7 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
-        for (String tapahtuma : Kirjanpito.getInstance().getTapahtumat()) {
+        for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
     }
