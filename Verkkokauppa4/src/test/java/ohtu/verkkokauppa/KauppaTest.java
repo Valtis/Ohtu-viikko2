194a5b9053862a47ac933b5eae30ad73062c0e7a
Valtis <erkka.kaaria@helsinki.fi>
2014-04-06 13:32:50 +0300

    Ensimmainen pyydetty testi

--- Verkkokauppa4/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Verkkokauppa4/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -63,4 +63,34 @@ public class KauppaTest {
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
     
+    
+    /* aloitetaan asiointi, koriin lisätään tuote ,jota varastossa on ja 
+    suoritetaan ostos (eli kutsutaan metodia kaupan tilimaksu()). varmistettava 
+    että kutsutaan pankin metodia tilisiirto oikealla asiakkaalla, tilinumerolla 
+    ja summalla  */
+    
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaanOikeallaParametreilla() {
+
+
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 1
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);   
+    }
+    
+    
 }
