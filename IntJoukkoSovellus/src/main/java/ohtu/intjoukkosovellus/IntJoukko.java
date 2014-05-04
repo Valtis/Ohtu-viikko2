1f2f48014560cac12244da8bb9792fe50c764470
Valtis <erkka.kaaria@helsinki.fi>
2014-05-04 14:21:05 +0300

    Yhdiste refaktoroitu

--- IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -129,16 +129,10 @@ public class IntJoukko {
     }
 
     public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
-        IntJoukko x = new IntJoukko();
-        int[] aTaulu = a.toIntArray();
-        int[] bTaulu = b.toIntArray();
-        for (int i = 0; i < aTaulu.length; i++) {
-            x.lisaa(aTaulu[i]);
-        }
-        for (int i = 0; i < bTaulu.length; i++) {
-            x.lisaa(bTaulu[i]);
-        }
-        return x;
+        IntJoukko ret = new IntJoukko();
+        ret.luvut.addAll(a.luvut);
+        ret.luvut.addAll(b.luvut);
+        return ret;
     }
 
     public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
