6e510848458616bb57e290a9adfb7fc7c8e5fa71
Valtis <erkka.kaaria@helsinki.fi>
2014-05-04 17:26:29 +0300

    Poistettiin tarpeettomia luokkia

--- QueryLanguage1/src/main/java/statistics/matcher/And.java
+++
@@ -1,9 +0,0 @@
-package statistics.matcher;
-
-import statistics.CompositeMatchStrategy.AndStrategy;
-
-class And extends CompositeBase {
-    public And(Matcher ...matchers) {
-        super(new AndStrategy(), matchers);
-    }
-}
