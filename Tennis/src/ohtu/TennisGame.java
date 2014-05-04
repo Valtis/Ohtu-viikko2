238f98f3975bcff75d5a2dedd4f69e26bd54927d
Valtis <erkka.kaaria@helsinki.fi>
2014-05-04 15:14:58 +0300

    Poistettiin for-looppi

--- Tennis/src/ohtu/TennisGame.java
+++ Tennis/src/ohtu/TennisGame.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TennisGame {
-       
+
     Map<Integer, String> scoreNames;
     Map<String, Integer> playerScores;
-    
+
     private int player1Score = 0;
     private int player2Score = 0;
     private String player1Name;
@@ -20,26 +20,22 @@ public class TennisGame {
         scoreNames.put(1, "Fifteen");
         scoreNames.put(2, "Thirty");
         scoreNames.put(3, "Forty");
-        
-        
-        
+
         playerScores.put(player1Name, 0);
         playerScores.put(player2Name, 0);
-        
-        
-        
-        
+
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
 
-        if (playerName.equals("player1"))
+        if (playerName.equals("player1")) {
             player1Score += 1;
-        else
+        } else {
             player2Score += 1;
-        
+        }
+
         if (playerScores.containsKey(playerName)) {
             playerScores.put(playerName, playerScores.get(playerName) + 1);
         }
@@ -47,45 +43,41 @@ public class TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (player1Score==player2Score)
-        {
-            switch (player1Score)
-            {
+        int tempScore = 0;
+        if (player1Score == player2Score) {
+            switch (player1Score) {
                 case 0:
-                        score = "Love-All";
+                    score = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score = "Thirty-All";
                     break;
                 case 3:
-                        score = "Forty-All";
+                    score = "Forty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    score = "Deuce";
                     break;
-                
+
             }
-        }
-        else if (player1Score>=4 || player2Score>=4)
-        {
-            int minusResult = player1Score-player2Score;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                score += scoreNames.get(tempScore);
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
             }
+        } else {
+            
+            score = scoreNames.get(player1Score) + "-" + scoreNames.get(player2Score);
+
         }
         return score;
     }
